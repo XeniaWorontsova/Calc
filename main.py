8bb476589094f86252c55813c1d1dbafed60259d
XeniaWorontsova <kv94441@gmail.com>
2019-10-25 23:29:02 +0300

    часть контроллера

--- main.py
+++ main.py
@@ -1,16 +1,30 @@
 import sys
 from design import Ui_Form
+from operations_manager import OperationsManager
 from PyQt5 import QtWidgets
 
 
-class ExampleApp(QtWidgets.QMainWindow, Ui_Form):
+class Controller(QtWidgets.QMainWindow, Ui_Form):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
+        self.operations_manager = OperationsManager()
+
+    def make_list_operations(self):
+        count = self.operations_manager.get_operations_count
+        i = 0
+        while i < count:
+            self.comboBox.addItem(self.operations_manager.get_operation(i))
+
+    def check_operand():
+        '''при change edit_line, но по заданию не так. 
+        Уточнить, зачем в контроллер передавать edit_line - это просто привязка к си шарпу? 
+        ПОПРОСИТЬ ОБЪЯСНИТЬ ВСЮ СТРУКТУРУ КОНТРОЛЛЕРА ЗАНОВО'''
+        
 
 def main():
     app = QtWidgets.QApplication(sys.argv)  # Новый экземпляр QApplication
-    window = ExampleApp()  # Создаём объект класса ExampleApp
+    window = Controller()  # Создаём объект класса Controller
     window.show()  # Показываем окно
     app.exec_()  # и запускаем приложение
 
