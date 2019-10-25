3f521582fbb24e72840f886d48c73bc278bffb02
XeniaWorontsova <kv94441@gmail.com>
2019-10-25 23:06:03 +0300

    добавлен менеджер операций

---
+++ operations_manager.py
@@ -0,0 +1,13 @@
+class OperationsManager():
+    def __init__(self):
+        self.__operations = []
+        self.__operations_count = 0
+
+    def get_operation(self, number):
+        return self.__operations[number]
+
+    def get_operations_count(self):
+        return self.__operations_count
+
+    def operations_load(self, path):
+        '''задание не понятно'''
